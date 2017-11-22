5b486f9affeed5a8011ac2bc478cf5f5e7787cc1
Alex Monastyretsky <alex.mon1989@gmail.com>
2017-11-22 20:48:53 +0200

    feature: вывод ближайших событий на главной странице.

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from django.utils import timezone
 from apps.news.models import News
+from apps.playbill.models import Event
 
 
 def home(request):
     """Отображает главную страницу сайта."""
     last_news = News.objects.filter(is_visible=True).order_by('-created_at')[:3]
+    last_events = Event.objects.filter(is_visible=True, datetime__gte=timezone.now()).order_by('datetime')[:10]
     return render(
         request,
         'home/index.html',
         {
-            'last_news': last_news
+            'last_news': last_news,
+            'last_events': last_events,
         }
     )
