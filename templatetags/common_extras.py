4f11e7fe1a5baa4724bd0555f28a0790c5069ab9
alexmon1989 <alex.mon1989@gmail.com>
2017-11-24 13:27:01 +0200

    fix: ошибка, если продолжительность спектакля не указана.

--- templatetags/common_extras.py
+++ templatetags/common_extras.py
@@ -33,11 +33,12 @@ def to_int(value):
 
 @register.filter
 def duration(td):
-    total_seconds = int(td.total_seconds())
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-
-    return '{} час. {} мин.'.format(hours, minutes)
+    if td:
+        total_seconds = int(td.total_seconds())
+        hours = total_seconds // 3600
+        minutes = (total_seconds % 3600) // 60
+        return '{} час. {} мин.'.format(hours, minutes)
+    return ''
 
 
 @register.filter
