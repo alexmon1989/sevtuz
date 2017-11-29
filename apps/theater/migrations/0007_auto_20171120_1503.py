68d4c9898bfa54df8ddb7d9c6b054a9fe7d72b50
Alex Monastyretsky <alex.mon1989@gmail.com>
2017-11-29 19:58:45 +0200

    fix: migration error.

--- apps/theater/migrations/0007_auto_20171120_1503.py
+++ apps/theater/migrations/0007_auto_20171120_1503.py
@@ -12,7 +12,11 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.RemoveField(
+            model_name='play',
+            name='duration',
+        ),
+        migrations.AddField(
             model_name='play',
             name='duration',
             field=models.DurationField(blank=True, null=True, verbose_name='Продолжительность, минут'),
