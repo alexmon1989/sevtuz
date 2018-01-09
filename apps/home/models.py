81ca66b48e6021df4dd11d46847f978f22e0ebc4
Alex Monastyretsky <alex.mon1989@gmail.com>
2018-01-09 23:02:30 +0200

    feature: афиша главной страницы ссылается на страницу спектакля.

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,20 +1,13 @@
 from django.db import models
-from django.utils import timezone
 from easy_thumbnails.fields import ThumbnailerImageField
-from apps.playbill.models import Event
-
-
-def limit_event_choices():
-    return {'datetime__gte': timezone.now()}
+from apps.theater.models import Play
 
 
 class Playbill(models.Model):
     """Модель афиши."""
-    event = models.ForeignKey(
-        Event,
-        verbose_name='Событие',
-        limit_choices_to=limit_event_choices,
-        help_text='Здесь показаны только будущие события'
+    play = models.ForeignKey(
+        Play,
+        verbose_name='Спектакль'
     )
     image = ThumbnailerImageField(
         'Изображение',
@@ -23,5 +16,5 @@ class Playbill(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Афиша главного события'
-        verbose_name_plural = 'Афиша главного события'
+        verbose_name = 'Афиша главной страницы'
+        verbose_name_plural = 'Афиша главной страницы'
