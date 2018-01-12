ace606ef188713d420d5e455212dab8525b23258
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2018-01-12 11:02:33 +0200

    feature: страница со списком страниц приложения media.

--- apps/media/urls.py
+++ apps/media/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from apps.media.views import PageDetailView
+from apps.media.views import PageDetailView, PageListView
 
 urlpatterns = [
+    url(r'^$', PageListView.as_view(), name='media_pages'),
     url(r'^(?P<slug>[-\w]+)/$', PageDetailView.as_view(), name='media_page'),
 ]
--- apps/media/views.py
+++ apps/media/views.py
@@ -1,9 +1,14 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView,ListView
 from apps.media.models import Page
 
 
 class PageDetailView(DetailView):
     """Отображает страницу раздела Медиа."""
     model = Page
-    template_name = "media/page_detail/page_detail.html"
+    queryset = Page.objects.filter(is_visible=True)
+
+
+class PageListView(ListView):
+    """Отображает страницу со списком сраниц."""
+    model = Page
     queryset = Page.objects.filter(is_visible=True)
