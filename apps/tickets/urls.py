fe457603ff99f66e88dc3fa9149d6c96d9536ea3
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2018-01-12 10:23:51 +0200

    feature: страница со списком страниц приложения tickets.

--- apps/tickets/urls.py
+++ apps/tickets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from apps.tickets.views import PageDetailView
+from apps.tickets.views import PageDetailView, PageListView
 
 urlpatterns = [
+    url(r'^$', PageListView.as_view(), name='tickets_pages'),
     url(r'^(?P<slug>[-\w]+)/$', PageDetailView.as_view(), name='tickets_page'),
 ]
--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -1,8 +1,14 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from apps.tickets.models import Page
 
 
 class PageDetailView(DetailView):
     """Отображает страницу приложения."""
+    model = Page
+    queryset = Page.objects.filter(is_visible=True)
+
+
+class PageListView(ListView):
+    """Отображает страницу со списком сраниц."""
     model = Page
     queryset = Page.objects.filter(is_visible=True)
